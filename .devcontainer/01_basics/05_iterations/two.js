56f2366ad75f40de4bb20315c982bb2d773684dc
Madhu-Yadav375 <myadav07012006@gmail.com>
2025-12-16 17:37:51 +0000

    iterations

---
+++ .devcontainer/01_basics/05_iterations/two.js
@@ -0,0 +1,22 @@
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+let arr = 0
+
+let myArr = ["flash", "thor", "ironman"]
+
+while (arr < myArr.length) {
+    console.log(myArr[arr]);
+    arr++;
+}
+
+
+let score = 11
+do {
+    console.log(`scor is ${score}`);
+    score++;
+} while (score<=10);
